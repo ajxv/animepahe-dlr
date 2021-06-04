e68a3d07b7e7a3e533fd5704767c0f84a1a5ca0e
dev <abin.xv@gmail.com>
2021-06-04 19:27:52 +0530

    minor fix

--- animepahe_dlr/ap_dlr_modules/initiate_driver.py
+++ animepahe_dlr/ap_dlr_modules/initiate_driver.py
@@ -15,7 +15,7 @@ parent_dir = os.path.dirname(this_dir) #path to parent of current dir
 current_system_os = str(platform.system()) #get current os
 
 #add geckodriver path to PATH
-geckodriver_path = os.path.join(os.path.join(this_dir, "ap_dlr_modules"), "geckodriver")
+geckodriver_path = os.path.join(this_dir, "geckodriver")
 if current_system_os.lower() == "windows":
     if os.path.exists(os.path.join(geckodriver_path, r"geckodriver.exe")):
         os.environ['PATH'] = os.environ['PATH'] + os.pathsep + geckodriver_path
@@ -34,12 +34,12 @@ if current_system_os.lower() == "windows": # we need this only in windows
 
 try:
     #initiate driver
-    driver = webdriver.Firefox(options=options)
+    driver = webdriver.Firefox(options=options, service_log_path=geckodriver_path)
 
 except WebDriverException as driverException:
     if "Message: 'geckodriver' executable needs to be in PATH." in str(driverException) :
         gecko_installer.install(this_dir) #installs and adds geckodriver to PATH
-        driver = webdriver.Firefox(options=options)
+        driver = webdriver.Firefox(options=options, service_log_path=geckodriver_path)
     else:
         print(driverException)
 
